c15b3888370adc6f3b8c86deffc46897fff7ae99
Vicey Wang <vicey@live.com>
2018-03-26 23:14:21 +0800

    Finished P0001

---
+++ 0001-0100/P0001/TwoSum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P0001
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (var slowerIndex = 0; slowerIndex < nums.Length; slowerIndex++)
+            {
+                for (var fasterIndex = slowerIndex + 1; fasterIndex < nums.Length; fasterIndex++)
+                {
+                    if (nums[slowerIndex] + nums[fasterIndex] == target)
+                        return new[] { slowerIndex, fasterIndex };
+                }
+            }
+            return null;
+        }
+    }
+}
