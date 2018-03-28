24f9dc18e5c939c3b79d23e86b8be62747748cd8
Vicey Wang <vicey@live.com>
2018-03-28 23:34:28 +0800

    Finished P0003

---
+++ 0001-0100/P0003/LengthOfLongestSubstring.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace P0003
+{
+    public class Solution
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            var indexTable = new int[256];
+            for (var i = 0; i < indexTable.Length; i++)
+                indexTable[i] = -1;
+
+            var length = 0;
+            var startIndex = 0;
+            var currentLength = 0;
+            for (var i = 0; i < s.Length; i++)
+            {
+                currentLength++;
+                var lastIndex = indexTable[s[i]];
+                indexTable[s[i]] = i;
+                if (lastIndex >= startIndex)
+                {
+                    length = Math.Max(length, currentLength - 1);
+                    startIndex = lastIndex + 1;
+                    currentLength = i - startIndex + 1;
+                }
+            }
+            length = Math.Max(length, currentLength - 1);
+            return length;
+        }
+    }
+}
--- 0001-0100/Runner/Program.cs
+++ 0001-0100/Runner/Program.cs
@@ -1,31 +1,31 @@
-﻿using P0002;
+﻿using P0003;
 
 namespace Runner
 {
     static class Program
     {
-        static ListNode CreateNode(int val)
-            => new ListNode(val);
 
-        static ListNode CreateList(params int[] vals)
-        {
-            if (vals.Length == 0) return null;
-            var current = CreateNode(vals[0]);
-            var start = current;
-            for (var i = 1; i < vals.Length; i++)
-            {
-                current.next = CreateNode(vals[i]);
-                current = current.next;
-            }
-            return start;
-        }
+        //static ListNode CreateNode(int val)
+        //    => new ListNode(val);
+
+        //static ListNode CreateList(params int[] vals)
+        //{
+        //    if (vals.Length == 0) return null;
+        //    var current = CreateNode(vals[0]);
+        //    var start = current;
+        //    for (var i = 1; i < vals.Length; i++)
+        //    {
+        //        current.next = CreateNode(vals[i]);
+        //        current = current.next;
+        //    }
+        //    return start;
+        //}
 
         static void Main(string[] args)
         {
-            var l1 = CreateList(0);
-            var l2 = CreateList(7, 3);
+            var str = "test";
             var sln = new Solution();
-            var result = sln.AddTwoNumbers(l1, l2);
+            sln.LengthOfLongestSubstring(str);
         }
     }
 }
