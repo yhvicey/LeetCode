c28537c71712d8b1fd24de9620c2ea149d0ae4d1
Vicey Wang <vicey@live.com>
2018-03-30 22:30:03 +0800

    Finished P0009

---
+++ 0001-0100/P0009/IsPalindrome.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace P0009
+{
+    public class Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0) return false;
+            long origin = x;
+            long compare = 0;
+            while (x != 0)
+            {
+                compare = compare * 10 + x % 10;
+                x /= 10;
+            }
+            return compare == Math.Abs(origin);
+        }
+    }
+}
