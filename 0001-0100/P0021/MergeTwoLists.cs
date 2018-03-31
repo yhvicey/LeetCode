d63b5057b216caeac13692ac4c3e2b855681c9fe
Vicey Wang <vicey@live.com>
2018-03-31 23:49:49 +0800

    P0021 WIP

---
+++ 0001-0100/P0021/MergeTwoLists.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P0021
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+
+    public class Solution
+    {
+        public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+
+        }
+    }
+}
